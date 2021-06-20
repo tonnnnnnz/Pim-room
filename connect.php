70cb64780c75b7b3086bdca9c2b3d5a17b169a66
tonnnnnnz <tonza2542@gmail.com>
2021-06-20 16:46:57 +0700

    Edit responsive

--- connect.php
+++ connect.php
@@ -8,7 +8,4 @@ if (!$conn) {
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
     exit;
 }
-//config
-// $org = "<i class='ace-icon glyphicon glyphicon-lock'></i>".'  Way IT Echo' ; //ชื่อหน่วยงาน
-// $boss = 'หัวหน้าสำนักปลัด' ; //ชื่อตำแหน่งหัวหน้าใช้ในปริ้นหนังสือ
 ?>
