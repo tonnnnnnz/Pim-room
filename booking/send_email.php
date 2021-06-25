d7e474e70cb4c6033c8b7050d68bbd9792cbc4e8
tonnnnnnz <tonza2542@gmail.com>
2021-06-26 01:23:09 +0700

    Edit send email

--- booking/send_email.php
+++ booking/send_email.php
@@ -30,40 +30,7 @@ $mail->Subject = $subject;
 
 // $email_content = "วันที่".$date ."\n";
 $email_content = "
-	<!DOCTYPE html>
-	<html>
-		<head>
-			<meta charset=utf-8'/>
-			<title>ทดสอบการส่ง Email</title>
-		</head>
-		<body>
-			<h1 style='background: #221f53;padding: 10px 0 10px 10px;margin-bottom:10px;font-size:30px;color:white;' >
-				<img src='https://upload.wikimedia.org/wikipedia/th/1/16/Logo_PIM.png' style='width: 45px; height: 40px;'>
-				PIM Room Reservation
-			</h1>
-			<div style='padding:20px;'>
-				<div>				
-					<h2>ข้อมูลการจอง : <strong style='color:#0000ff;'></strong></h2>
-					<p>ยืนยันการจองห้องประชุม ".$room."</p>
-					<p>วันที่ ".$date." เวลา ".$startt." - ".$endt." น. สำเร็จ</p>
-					<p>กรุณาติดต่อเคาน์เตอร์บริการก่อนเข้าใช้งาน</p>
-				</div>
-				<div style='margin-top:30px;'>
-					<hr>
-					<address>
-						<h4>ติดต่อสอบถาม</h4>
-						<p>ห้องสมุดสถาบันการจัดการปัญญาภิวัฒน์</p>
-						<p>Tel: 0 2855 0381-82</p>
-					</address>
-				</div>
-			</div>
-			<div style='background: #221f53;color: #dddada;padding:10px;'>
-				<div style='text-align:center'> 
-					2021 © PIM Room Reservation
-				</div>
-			</div>
-		</body>
-	</html>
+	test
 ";
 
 //  ถ้ามี email ผู้รับ
@@ -71,7 +38,6 @@ if($email_receiver){
 	$mail->msgHTML($email_content);
 
 	if (!$mail->send()) {  // สั่งให้ส่ง email
-
 		// กรณีส่ง email ไม่สำเร็จ
 		echo "<h3 class='text-center'>ระบบมีปัญหา กรุณาลองใหม่อีกครั้ง</h3>";
 		echo $mail->ErrorInfo; // ข้อความ รายละเอียดการ error
